fe71b4b6bb20f2e12475faa02470d4b3e990b417
cipepser <respepic@gmail.com>
2018-10-08 21:15:12 +0900

    add type LetStatement and Identifier, and implement their mthods

--- 02/src/monkey/ast/ast.go
+++ 02/src/monkey/ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "github.com/cipepser/goInterpreter-sample/02/src/monkey/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -24,4 +26,27 @@ func (p *Program) TokenLiteral() string {
 	} else {
 		return ""
 	}
+}
+
+type LetStatement struct {
+	Token token.Token // token.LET
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token // token.IDENT
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
