3aa59480781d0a90a77657d5685fdf89e15f26fe
cipepser <respepic@gmail.com>
2018-10-27 12:16:24 +0900

    implement peekError

--- 02/src/monkey/parser/parser.go
+++ 02/src/monkey/parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/cipepser/goInterpreter-sample/02/src/monkey/ast"
 	"github.com/cipepser/goInterpreter-sample/02/src/monkey/lexer"
 	"github.com/cipepser/goInterpreter-sample/02/src/monkey/token"
@@ -9,16 +11,31 @@ import (
 type Parser struct {
 	l *lexer.Lexer
 
+	errors []string
+
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -87,6 +104,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
