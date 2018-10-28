3bf5b203b45048c9929ca81d96ad1ab4e3e91453
cipepser <respepic@gmail.com>
2018-10-28 21:16:11 +0900

    add FunctionExpression

--- 02/src/monkey/ast/ast.go
+++ 02/src/monkey/ast/ast.go
@@ -2,6 +2,7 @@ package ast
 
 import (
 	"bytes"
+	"strings"
 
 	"github.com/cipepser/goInterpreter-sample/02/src/monkey/token"
 )
@@ -257,3 +258,32 @@ func (bs *BlockStatement) String() string {
 }
 
 func (bs *BlockStatement) statementNode() {}
+
+type FunctionLiteral struct {
+	Token      token.Token // 'fn' token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) TokenLiteral() string {
+	return fl.Token.Literal
+}
+
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(")")
+	out.WriteString(fl.Body.String())
+
+	return out.String()
+}
+
+func (fl *FunctionLiteral) expressionNode() {}
