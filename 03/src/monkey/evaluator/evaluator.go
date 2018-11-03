88995cb87d1af6675efa8f36ce1d5b30595e40cd
cipepser <respepic@gmail.com>
2018-11-03 14:28:47 +0900

    use global object for Boolean

--- 03/src/monkey/evaluator/evaluator.go
+++ 03/src/monkey/evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"github.com/cipepser/goInterpreter-sample/03/src/monkey/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
@@ -17,7 +22,7 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 
 	case *ast.Boolean:
-		return &object.Boolean{Value: node.Value}
+		return nativeBoolToBooleanObject(node.Value)
 
 	}
 
@@ -32,4 +37,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
